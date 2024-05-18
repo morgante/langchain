535c3e1f54e03e444792edb3c1b24fa5013b2449
Morgante Pell <morgantep@google.com>
2024-05-18 14:02:56 -0700

    [skip ci] yolo

--- libs/cli/langchain_cli/namespaces/migrate/generate/grit.py
+++ libs/cli/langchain_cli/namespaces/migrate/generate/grit.py
@@ -1,6 +1,27 @@
 from typing import List, Tuple
 
+def split_package(package: str) -> Tuple[str, str]:
+    """Split a package name into the containing package and the final name"""
+    parts = package.split(".")
+    return ".".join(parts[:-1]), parts[-1]
 
 def dump_migrations_as_grit(name: str, migration_pairs: List[Tuple[str, str]]):
     """Dump the migration pairs as a Grit file."""
-    return "\n".join([f"{from_module} -> {to_module}" for from_module, to_module in migration_pairs])
+    output = "language python"
+    remapped = ",\n".join([
+        f"    [`{split_package(from_module)[0]}`, `{split_package(from_module)[1]}`, `{split_package(to_module)[0]}`, `{split_package(to_module)[1]}`]"
+        for from_module, to_module in migration_pairs
+    ])
+    output = f"""
+language python
+
+pattern {name}() {{
+  find_replace_imports(list=[
+{remapped}
+  ])
+}}
+
+# Add this for invoking directly
+{name}()
+"""
+    return output
